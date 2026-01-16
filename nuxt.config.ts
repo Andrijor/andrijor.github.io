6d605437ed5b66ecc38b7fe4a753e6c99601c6b3
Andrijor <andrijor@gmail.com>
2026-01-16 03:40:36 +0100

    disabled devtools in dev mode because it was bothering me

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   ssr: true,
   modules: ["@nuxt/eslint", "@nuxt/icon"],
   vite: {

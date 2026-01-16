d99514b80d04b73aebaff1687b505378c923b2ba
Andrijor <andrijor@gmail.com>
2026-01-16 15:29:18 +0100

    Made all of the index contents responsive in a single element instead of two

--- app/components/IndexContentsComponent.vue
+++ app/components/IndexContentsComponent.vue
@@ -1,43 +1,21 @@
 <template>
   <div
-    id="computer-contents"
-    class="hidden sm:grid sm:grid-cols-2 xl:grid-cols-[1fr_1fr]"
+    class="sm:grid sm:grid-cols-2 sm:grid-rows-1 grid-rows-[2fr_3fr] pb-4 gap-y-1 items-center h-full"
   >
-    <div class="pr-12 flex flex-col justify-center text-right">
-      <h1 class="text-7xl mb-8">Andrés J.</h1>
-      <h2 class="text-3xl">Studying Computer Science (MSc.)</h2>
-      <h3 class="text-2xl">TU Berlin, Germany</h3>
-      <p class="text-xl mt-8">Currently looking for a subject for my thesis!</p>
-      <p class="text-xl">
+    <div
+      class="sm:pr-12 p-4 flex flex-col justify-center sm:text-right sm:self-center text-left self-end"
+    >
+      <h1 class="text-4xl mb-2 sm:text-7xl sm:mb-8">Andrés J.</h1>
+      <h2 class="text-2xl sm:text-3xl">Studying Computer Science (MSc.)</h2>
+      <h3 class="text-lg sm:text-2xl">TU Berlin, Germany</h3>
+      <p class="text-lg mt-2 sm:text-xl sm:mt-8">
+        Currently looking for a subject for my thesis!
+      </p>
+      <p class="text-lg sm:text-xl">
         Please check out the interests section for potential thesis topics.
       </p>
     </div>
-    <div class="grid py-4 gap-y-4 grid-template-rows-[repeat(3, 1fr)]">
-      <div>
-        <LinkCardComponent category="interests" />
-      </div>
-      <div>
-        <LinkCardComponent category="skills" />
-      </div>
-      <div>
-        <LinkCardComponent category="cv" />
-      </div>
-    </div>
-  </div>
-  <div
-    id="mobile-contents"
-    class="grid grid-rows-[2fr_3fr] sm:hidden pb-4 gap-y-1 items-center h-full"
-  >
-    <div class="p-4 flex flex-col justify-center text-left self-end">
-      <h1 class="text-4xl mb-2">Andrés J.</h1>
-      <h2 class="text-2xl">Studying Computer Science (MSc.)</h2>
-      <h3 class="text-lg">TU Berlin, Germany</h3>
-      <p class="text-lg mt-2">Currently looking for a subject for my thesis!</p>
-      <p class="text-lg">
-        Please check out the interests section for potential thesis topics.
-      </p>
-    </div>
-    <div class="grid grid-rows-3 gap-y-4">
+    <div class="grid grid-rows-3 gap-y-4 py-4 h-full">
       <LinkCardComponent category="interests" />
       <LinkCardComponent category="skills" />
       <LinkCardComponent category="cv" />
